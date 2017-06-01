bb58273f4864e8ec5c6f0f50900c561c68c15bbe
AtikaFarhana <atifarha091@gmail.com>
2017-06-01 07:40:14 +0600

    add sign up activity

---
+++ app/src/main/java/com/example/sam/finalproject/FirebaseApplication.java
@@ -0,0 +1,53 @@
+package com.example.sam.finalproject;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by AtiAfi on 6/1/2017.
+ */
+
+public class FirebaseApplication extends Application {
+
+    private static final String TAG = FirebaseApplication.class.getSimpleName();
+
+    public FirebaseAuth firebaseAuth;
+
+    public FirebaseAuth.AuthStateListener mAuthListener;
+
+    public FirebaseAuth getFirebaseAuth() {
+        return firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public String getFirebaseAuthenticationId(){
+        String userId = null;
+        if(firebaseAuth.getCurrentUser() != null){
+
+            userId = firebaseAuth.getCurrentUser().getUid();
+        }
+
+        return userId;
+    }
+
+    public void checkUserLogin(final Context context){
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (null != user){
+
+                    Intent profileIntent = new Intent (context,ProfileActivity.class);
+                }
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/sam/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/sam/finalproject/LoginActivity.java
@@ -1,13 +1,46 @@
 package com.example.sam.finalproject;
 
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private static  final String TAG = LoginActivity.class.getSimpleName();
+
+    private EditText emailInput;
+    private EditText passwordInput;
+
+    private TextView signUpTV;
+    private TextView loginError;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.hide();
+        }
+
+        mAuth = ( (FirebaseApplication) getApplication()).getFirebaseAuth();
     }
 }
