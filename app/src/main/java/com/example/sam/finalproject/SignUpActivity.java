3e53a843337f5b7a876dd312d245418fbb7a9ebe
AtikaFarhana <atifarha091@gmail.com>
2017-06-05 09:14:28 +0600

    update sign up file

--- app/src/main/java/com/example/sam/finalproject/SignUpActivity.java
+++ app/src/main/java/com/example/sam/finalproject/SignUpActivity.java
@@ -1,7 +1,14 @@
 package com.example.sam.finalproject;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -22,7 +29,41 @@ public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+
+        mAuth = ((FirebaseApplication)getApplication()).getFirebaseAuth();
+
+        ((FirebaseApplication)getApplication()).checkUserLogin(SignUpActivity.this);
+
+        loginError = (TextView) findViewById(R.id.login_error);
+
+        emailInput = (EditText)findViewById(R.id.email);
+        passwordInput = (EditText)findViewById(R.id.password);
+
+        signUpTV = (TextView)findViewById(R.id.register);
+
+        signUpTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUpIntent = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(signUpIntent);
+            }
+        });
+
+        final Button loginButton = (Button)findViewById(R.id.login_button);
+        
     }
 }
