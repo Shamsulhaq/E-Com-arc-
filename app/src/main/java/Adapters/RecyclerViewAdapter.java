f2040350f6807dcc26b6af33b639858ea72f9a92
AtikaFarhana <atifarha091@gmail.com>
2017-06-05 12:33:51 +0600

    add recyclerViewwAdapter file

--- app/src/main/java/Adapters/RecyclerViewAdapter.java
+++ app/src/main/java/Adapters/RecyclerViewAdapter.java
@@ -1,8 +1,47 @@
 package Adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.sam.finalproject.R;
+import com.example.sam.finalproject.UserProfile;
+
+import java.util.List;
+
 /**
  * Created by AtiAfi on 6/5/2017.
  */
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolders> {
+
+    private List<UserProfile> user;
+    protected Context context;
+    public RecyclerViewAdapter(Context context, List<UserProfile> user) {
+        this.user = user;
+        this.context = context;
+    }
+    @Override
+    public RecyclerViewHolders onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        RecyclerViewHolders viewHolder = null;
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.profile_data_list, parent, false);
+        viewHolder = new RecyclerViewHolders(layoutView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerViewHolders holder, int position) {
+
+        holder.profileHeader.setText(user.get(position).getHeader());
+        holder.profileContent.setText(user.get(position).getProfileContent());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.user.size();
+    }
 }
