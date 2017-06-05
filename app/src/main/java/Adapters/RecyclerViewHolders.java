eec4d20212104dd9d490671fb0f7a4a9d293ef1e
AtikaFarhana <atifarha091@gmail.com>
2017-06-05 12:34:31 +0600

    add recyclerViewwHolder file

--- app/src/main/java/Adapters/RecyclerViewHolders.java
+++ app/src/main/java/Adapters/RecyclerViewHolders.java
@@ -1,8 +1,26 @@
 package Adapters;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.sam.finalproject.R;
+
 /**
  * Created by AtiAfi on 6/5/2017.
  */
 
-public class RecyclerViewHolders {
+public class RecyclerViewHolders extends RecyclerView.ViewHolder {
+
+    private static final String TAG = RecyclerViewHolders.class.getSimpleName();
+
+    public TextView profileHeader;
+
+    public TextView profileContent;
+
+    public RecyclerViewHolders(View itemView) {
+        super(itemView);
+        profileHeader = (TextView)itemView.findViewById(R.id.heading);
+        profileContent = (TextView) itemView.findViewById(R.id.profile_content);
+    }
 }
